163da6a90e1e28d02f367f053095c0e8c1119077
Siul Orutra <orutra.siul_mc@hotmail.com>
2022-08-15 16:49:06 -0500

    [Added] firebase configured

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('index.html', (error, data) => {
+    fs.readFile('public/index.html', (error, data) => {
         if (error) {
             res.writeHead(404);
             res.write('Error: No se encontró el archivo que estás buscando');
