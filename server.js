591bfb3b6be7a6166f6ff1e0ace5a7114fd61cd1
Siul Orutra <orutra.siul_mc@hotmail.com>
2022-08-15 14:54:44 -0500

    [Added] moving cube test with marker tacking

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', (error, data) => {
+        if (error) {
+            res.writeHead(404);
+            res.write('Error: No se encontró el archivo que estás buscando');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+});
+
+server.listen(port, error => {
+    if (error) {
+        console.log('Ocurrio un error ', error);
+    } else {
+        console.log(`Servidor corriendo en localhost:${port}`);
+    }
+});
